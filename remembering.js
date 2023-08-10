275f6ca75c928a0f11aa748ee33998b97f10dd2b
iagomoraisn <iagojkd96@hotmail.com>
2023-08-09 23:52:06 -0300

    ternario

--- remembering.js
+++ remembering.js
@@ -1,4 +1,4 @@
-var msg = document.createElement("p");
+/* var msg = document.createElement("p");
 msg.textContent = "This is a message box";
 panel.appendChild(msg);
 
@@ -8,4 +8,15 @@ panel.appendChild(closeBtn);
 
 closeBtn.onClick = function () {
     panel.parentNode.removeChild(panel);
-};
+}; */
+
+//operador ternario
+//var resultado = (condição) ? valor1 : valor2
+
+/* let enviar_email = false
+let frase = enviar_email ? "Enviar" : "Não enviar"
+console.log(frase) */
+/* 
+let valor = 10
+let frase = valor < 5 ? 'sim' : 'não'
+console.log(frase) */
