98db1ac7fafd79ae2cff521f57eadaaf98364bf3
iagomoraisn <iagojkd96@hotmail.com>
2023-09-27 14:26:43 -0300

    node assincrono

---
+++ brlValue.js
@@ -0,0 +1,6 @@
+const brl = 5.37;
+const usdToBrl = (valueInUsd) => valueInUsd * brl;
+module.exports = {
+    brl,
+    usdToBrl,
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const { brl, usdToBrl } = require('./brValue');
+
+console.log(brValue);
+console.log(`Valor do dólar: ${brValue.brl}`);
+console.log(`10 dólares em reais: ${brValue.usdToBrl(10)}`);
