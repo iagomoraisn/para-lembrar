7093f92bee15f9dc67c7ee742d4c68ed3ef4a223
iagomoraisn <iagojkd96@hotmail.com>
2023-10-01 13:03:15 -0300

    criando scripts

--- my-scripts/bmi.js
+++ my-scripts/bmi.js
@@ -1,23 +1,23 @@
 const readline = require('readline-sync');
 
-const weightInKg = 80;
-const heightInCm = 178;
+/* const weightInKg = 80;
+const heightInCm = 178; */
 
 function handleBMI(weight, height) {
     console.log(`Weight: ${weight}, Height: ${height}`);
     const heightInMeters = height / 100;
     const heightSquared = heightInMeters ** 2;
 
-    const bmi = weight / heightSquared;
+    const bmi = (weight / heightSquared);
 
     return bmi;
 }
 
 function main() {
-    const weight = readline.questionInt('What\'s your weight? (kg) ');
+    const weight = readline.questionFloat('What\'s your weight? (kg) ');
     const height = readline.questionInt('What\'s your height? (cm) ');
     
-    const bmi = handleBMI(weightInKg, heightInCm);
+    const bmi = handleBMI(weight, height);
     console.log(`BMI: ${bmi.toFixed(2)}`);
 }
 
