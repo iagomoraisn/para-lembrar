327920f10c4cbfc1af7535382ffcb9eff3b70106
iagomoraisn <iagojkd96@hotmail.com>
2023-10-02 23:19:02 -0300

    utilizando GET retornando total de chocolates

--- cacau-trybe/tests/integration/chocolates.test.js
+++ cacau-trybe/tests/integration/chocolates.test.js
@@ -225,5 +225,15 @@ const mockFile = JSON.stringify({
         });
     
       });
+
+      describe('Usando o método GET em /chocolates/total', function () {
+        it('Retorna a quantidade total de chocolates', async function () {
+          const response = await chai.request(app)
+          .get('/chocolates/total');
+
+          expect(response.status).to.be.equal(200);
+          expect(response.body).to.deep.equal({ totalChocolates: 4 });
+        });
+      });
   
   });
