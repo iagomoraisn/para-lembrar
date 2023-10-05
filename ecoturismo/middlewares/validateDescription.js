9e4bd8546368326d2097329f13325f66ea418420
iagomoraisn <iagojkd96@hotmail.com>
2023-10-05 14:42:58 -0300

    validateDescription

--- ecoturismo/middlewares/validateDescription.js
+++ ecoturismo/middlewares/validateDescription.js
@@ -5,4 +5,24 @@ if (!description) {
         { message: 'O campo description é obrigatorio' },
     );
 }
-};
+
+if (!description.createdAt) {
+    return res.status(400).json(
+        { message: 'O campo createdAt é obrigatorio' },
+    );
+}
+
+if (!description.rating) {
+    return res.status(400).json(
+        { message: 'O campo rating é obrigatorio' },
+    );
+}
+
+if (!description.difficulty) {
+    return res.status(400).json(
+        { message: 'O campo difficulty é obrigatorio' },
+    );
+}
+
+next();
+}; //parte 7
